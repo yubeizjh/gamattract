d530455669014770b7572d15ed9e2e91013ea264
yubeizjh <b00763378@essec.edu>
2022-03-07 10:04:01 +0100

    setup

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,49 @@
-<script>
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
-</script>
+<div class="content">
+    <nav>
+        <span class="company-title">Company Name</span>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+        </ul>
+        <div>
+            <a href="/login">Login</a>
+        </div>
+    </nav>
 
-<Header />
-
-<main>
-	<slot />
-</main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+    <slot></slot>
+</div>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
+    .content {
+        padding: 20px 10px;
+    }
 
-	footer a {
-		font-weight: bold;
-	}
+    nav {
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+    ul {
+        list-style-type: none;
+        display: flex;
+        gap: 60px;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    .company-title {
+        font-size: large;
+        font-weight: bold;
+    }
 </style>
