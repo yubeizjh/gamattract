1327bb4928eae199c911ef1c2bb9cdd6b6a16a96
yubeizjh <b00763378@essec.edu>
2022-03-07 13:46:26 +0100

    bug fix

---
+++ src/routes/details/gD_dark_soul_3.svelte
@@ -0,0 +1,29 @@
+
+<!-- card entry -->
+<div class="w3-row-padding w3-card-4 w3-margin w3-white w3-round">
+<!-- block -->
+<div style="margin-top:15px"></div>
+<!-- main pics -->
+<div class="w3-col l6 m6 w3-margin-bottom ">
+    <img src="../gameImages/dark-souls-iii.jpeg" alt="John" style="width:100%">
+</div>
+<!-- information -->
+<div class="w3-col l6 m6 w3-margin-bottom">
+    <h3><em>Dark Soul III</em></h3>
+    <p class="w3-opacity">Dark Souls III is a 2016 action role-playing video game developed by FromSoftware and published by Bandai Namco Entertainment for PlayStation 4, Xbox One, and Microsoft Windows. It is the fourth overall entry of the Souls series and the final installment of the Dark Souls trilogy.</p>
+    <p>Release date: March 24, 2016</p>
+    <p>Platform: PC, PS4, PS5, Xbox X, Xbox S</p>
+    <p>Developer: FromSoftware Inc.</p>
+</div>
+<div class="w3-margin-left">
+    <h5>&nbsp<br><br>Related Feeds</h5>
+    <div class="w3-card-4 w3-white w3-round-xxlarge w3-margin-bottom">
+    <p class="w3-padding-16 w3-center">Not ready yet</p>
+    </div>
+</div>
+<div class="w3-center">
+    <a class="w3-round-xxlarge w3-button w3-black w3-section" href="../gallery.html"> 
+    <i class="fa fa-cubes"></i> back to galary
+    </a>
+</div>
+</div>
---
+++ src/routes/details/gameDetails_01.svelte
@@ -0,0 +1,32 @@
+
+<!-- card entry -->
+<div class="w3-card-4 w3-white w3-round">
+<div class="w3-row-padding">
+    <!-- block -->
+    <div style="margin-top:15px"></div>
+    <!-- main pics -->
+    <div class="w3-col l6 m6 w3-margin-bottom ">
+    <img src="../gameImages/elden-ring.jpeg" alt="John" style="width:100%">
+    </div>
+    <!-- information -->
+    <div class="w3-col l6 m6 w3-margin-bottom">
+    <h3><em>Elden Ring</em></h3>
+    <p class="w3-opacity">Elden Ring is an open-world action role-playing developed by FromSoftware and published by Bandai Namco Entertainment. The game, directed by Hidetaka Miyazaki, was made in collaboration with fantasy novelist George R. R. Martin, who provided material for the game's setting.</p>
+    <p>Release date: February 25, 2022</p>
+    <p>Platform: PC, PS4, PS5, Xbox X, Xbox S</p>
+    <p>Developer: FromSoftware Inc.</p>
+    <p>Composer: Yuka Kitamura</p>
+    </div>
+</div>
+<div class="w3-margin">
+    <h5>Related Feeds</h5>
+    <div class="w3-card-4 w3-white w3-round-xxlarge w3-margin-bottom">
+    <a href="../feeds/feedsDetail_01.html"><p class="w3-padding-16 w3-center">Elden Ring Review</p></a>
+    </div>
+</div>
+<div class="w3-center">
+    <a class="w3-round-xxlarge w3-button w3-black w3-section" href="../gallery.html"> 
+    <i class="fa fa-cubes"></i> back to galary
+    </a>
+</div>
+</div>
