20eaab7cda1478dfdaa7939461aee7e51ad9afd1
yubeizjh <b00763378@essec.edu>
2022-03-14 12:14:06 +0100

    small page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,8 @@
 <!-- Navigator -->
 <div class="w3-top">
     <div class="w3-bar w3-black w3-card">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" on:click={myFunction} title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
         <a href="/" class="w3-bar-item w3-button w3-padding-large"><img src="logo.png" alt="logo" style="height: 15px; margin-top: -3px;"></a>
         <a href="/" class="w3-bar-item w3-button w3-padding-large">LATEST</a>
         <a href="/gallery" class="w3-bar-item w3-button w3-padding-large w3-hide-small">COLLECTION</a>
@@ -22,7 +24,19 @@
         <a target="_blank" href="https://gameattract.wordpress.com/" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-thumbs-up"></i></a>
     </div>
 </div>
-    
+
+<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
+<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+
+    <a href="/" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>LATEST</a>
+    <a href="/gallery" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>COLLECTION</a>
+    <a href="/gameFeeds" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>FEEDS</a>
+    <a href="/moments" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>BEST MOMENTS</a>
+    <a href="/websiteGuide" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>WEBSITES</a>
+    <a href="/about" class="w3-bar-item w3-button w3-padding-large" on:click={myFunction}>CONTACT</a>
+
+  </div>
+
 <!-- Page content -->
 <div class="w3-content" style="max-width:2000px; margin-top:46px">
     <slot/>
@@ -34,3 +48,14 @@
     <a href="https://twitter.com/essec" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
     <p class="w3-medium">Powered by <a href="https://www.essec.edu/en/" target="_blank">ESSEC</a></p>
 </footer>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+</script>
