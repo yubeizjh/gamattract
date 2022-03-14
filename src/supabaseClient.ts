aa26c3e63e694a61237a7f57cbf9f93b9bb954c4
yubeizjh <b00763378@essec.edu>
2022-03-14 11:46:48 +0100

    contact new

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SVELTE_APP_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SVELTE_APP_SUPABASE_ANON_KEY as string;
+const supabaseUrl = "https://yheumjehjtqixrgmdvnw.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZXVtamVoanRxaXhyZ21kdm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDYwNDU1MzEsImV4cCI6MTk2MTYyMTUzMX0.ma7ocFuAIOUus7fCDH0aBNfdXfsNlVDNF50Bs_PUpUo";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
